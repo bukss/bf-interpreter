00a919bf9207ed373b4e59a8192b61092044043e
ldesgoui <ldesgoui@ldesgoui.xyz>
2020-01-21 01:18:14 +0100

    how I would

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,148 +1,128 @@
 use std::collections::HashMap;
 
-fn throw (error: String) {
-    println!("{}", error);
-    std::process::exit(0);
-}
-
+#[derive(Debug)]
 pub enum Instruction {
     Increment,
     Decrement,
     MoveLeft,
     MoveRight,
-    StartLoop(u32), // u32 is the index of the end of the loop
-    EndLoop(u32),   // u32 is the index of the start of the loop
+    StartLoop(usize), // usize is the index of the end of the loop
+    EndLoop(usize),   // usize is the index of the start of the loop
     Output,
     Input,
     Halt,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum BuildLoopMapError {
+    #[error("found loops with missing close instructions at indices {0:?}")]
+    StrayOpens(Vec<usize>),
+    #[error("found loop with missing open instructions at index {0}")]
+    StrayClose(usize),
+}
+
+// Creates mapping of loops and their endpoints for easy jumping around the code at loops endpoints and startpoints
+fn build_loop_map(code: &str) -> Result<HashMap<usize, usize>, BuildLoopMapError> {
+    use BuildLoopMapError::*;
+
+    let mut loops = HashMap::new();
+    let mut open_loops = Vec::new();
+
+    for (i, c) in code.chars().enumerate() {
+        match c {
+            '[' => open_loops.push(i),
+            ']' => {
+                let start = open_loops.pop().ok_or_else(|| StrayClose(i))?;
+
+                loops.insert(start, i);
+                loops.insert(i, start);
+            }
+            _ => {}
+        }
+    }
+
+    if !open_loops.is_empty() {
+        return Err(StrayOpens(open_loops));
+    }
+
+    Ok(loops)
+}
+
+// Converts the code string into a vector of instructions
+pub fn build_instructions(code: &str, loops: &HashMap<usize, usize>) -> Vec<Instruction> {
+    let mut instructions = Vec::new();
+
+    for (i, c) in code.chars().enumerate() {
+        let instruction = match c {
+            '+' => Instruction::Increment,
+            '-' => Instruction::Decrement,
+            '<' => Instruction::MoveLeft,
+            '>' => Instruction::MoveRight,
+            '[' => Instruction::StartLoop(*loops.get(&i).unwrap()),
+            ']' => Instruction::EndLoop(*loops.get(&i).unwrap()),
+            '.' => Instruction::Output,
+            ',' => Instruction::Input,
+            _ => continue,
+        };
+
+        instructions.push(instruction);
+    }
+
+    instructions.push(Instruction::Halt);
+
+    instructions
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum NewInterpreterError {
+    #[error("error while building loop map")]
+    BuildLoopMap(#[from] BuildLoopMapError),
+}
+
 pub struct Interpreter {
     pub instructions: Vec<Instruction>,
-    pub pointer: usize,
-    pub code: String,
-    pub loops: HashMap<u32, u32>,
-    pub input: String,
-    pub tape: [u8; 3000]
+    pub instruction_pointer: usize,
+    pub data_pointer: usize,
+    pub tape: [u8; 3000],
 }
 
 impl Interpreter {
-    fn build_loop_map (&mut self) {
-        /* Creates mapping of loops and their endpoints for easy jumping around the code at loops
-         * endpoints and startpoints */
+    pub fn new(code: &str) -> Result<Self, NewInterpreterError> {
+        let loops = build_loop_map(code)?;
+        let instructions = build_instructions(code, &loops);
 
-        let mut open_loops: Vec<u32> = Vec::new();
-        for (i, c) in self.code.chars().enumerate() {
-            let i: u32 = i as u32;
-            if c == '[' {
-                open_loops.push(i)
-            } else if c == ']' {
-                let start = match open_loops.pop() {
-                    Some(n) => n,
-                    None    => {
-                        throw(format!("Loop Error: Closure of nonexistent loop\nChar: ']' Pos: {}", i));
-                        0
-                    }
-                };
-                self.loops.insert(start, i);
-                self.loops.insert(i, start);
-            }
-        }
-        if open_loops.len() > 0 {
-            throw(format!("Loop Error: At least one unclosed loop\nChar: '[', Pos: {}", open_loops[0]));
-        }
-    }
-    
-    pub fn build_instructions (&mut self) { 
-        /* Converts the code string into a vector of instructions  */
-        
-        self.build_loop_map();
-        for (i, c) in self.code.chars().enumerate() {
-            let i: u32 = i as u32;
-            if c == '+' {
-                self.instructions.push(Instruction::Increment);
-            
-            } else if c == '-' {
-                self.instructions.push(Instruction::Decrement);
-            
-            } else if c == '<' {
-                self.instructions.push(Instruction::MoveLeft);
-
-            } else if c == '>' {
-                self.instructions.push(Instruction::MoveRight);
-            
-            } else if c == '[' {
-                let end = self.loops.get(&i).unwrap();
-                self.instructions.push(Instruction::StartLoop(*end));
-
-            } else if c == ']' {
-                let start = self.loops.get(&i).unwrap();
-                self.instructions.push(Instruction::EndLoop(*start));
-            
-            } else if c == '.' {
-                self.instructions.push(Instruction::Output);
-            
-            } else if c == ',' {
-                self.instructions.push(Instruction::Input);
-            }
-        }
-        self.instructions.push(Instruction::Halt);
+        Ok(Self {
+            instructions,
+            instruction_pointer: 0,
+            data_pointer: 0,
+            tape: [0; 3000],
+        })
     }
 
-    pub fn run(&mut self) {
-        /* Loops over the vec of instructions and executes the corresponding code */
-        let tape_size: usize = self.tape.len() - 1; // Represents the last index of the tape rather than the len
-        let mut input_pointer: usize = 0;
-        let inputs: Vec<u8> = self.input.clone().into_bytes();
-        let mut instruction_pointer: usize = 0;
-        loop {
-            let instruction = &self.instructions[instruction_pointer]; 
-            let cell = &mut self.tape[self.pointer]; 
-            match *instruction {
-                Instruction::Increment => {
-                    *cell = cell.overflowing_add(1).0; 
-                },
-                Instruction::Decrement => {
-                    *cell = cell.overflowing_sub(1).0;
-                },
-                Instruction::MoveRight => {
-                    self.pointer = self.pointer.overflowing_sub(1).0;
-                    if self.pointer > tape_size {
-                        self.pointer = tape_size;
-                    }
-                }
-                Instruction::MoveLeft => {
-                    self.pointer += 1;
-                    if self.pointer > tape_size {
-                        self.pointer = 0;
-                    }
-                },
-                Instruction::StartLoop(n) => {
-                    if *cell == 0 {
-                        instruction_pointer = n as usize;
-                    }   
-                },
-                Instruction::EndLoop(n) => {
-                    if *cell != 0 {
-                        instruction_pointer = n as usize;
-                    }
-                },
-                Instruction::Output => {
-                    print!("{}", *cell as char);
-                },
-                Instruction::Input => {
-                    let c = match inputs.get(input_pointer) {
-                        Some(a) => *a,
-                        None    => 0
-                    };
-                    *cell = c;
-                    input_pointer += 1;
-                },
-                Instruction::Halt => {
-                    break;
-                }
-            }
-        instruction_pointer += 1;
+    // Loops over the vec of instructions and executes the corresponding code
+    pub fn run(&mut self, mut input: impl Iterator<Item = u8>) {
+        while self.step(&mut input) {}
+    }
+
+    pub fn step(&mut self, mut input: impl Iterator<Item = u8>) -> bool {
+        use Instruction::*;
+
+        let cell = &mut self.tape[self.data_pointer];
+        match self.instructions[self.instruction_pointer] {
+            Increment => *cell = cell.wrapping_add(1),
+            Decrement => *cell = cell.wrapping_sub(1),
+            MoveRight => self.data_pointer += 1,
+            MoveLeft => self.data_pointer -= 1,
+            StartLoop(n) if *cell == 0 => self.instruction_pointer = n - 1,
+            EndLoop(n) if *cell != 0 => self.instruction_pointer = n - 1,
+            Output => print!("{}", *cell as char),
+            Input => *cell = input.next().unwrap(),
+            Halt => return false,
+            _ => {}
         }
+
+        self.instruction_pointer += 1;
+
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,32 @@
-use std::{collections::HashMap,
-    io::stdin};
+use anyhow::Context;
+use interpreter::Interpreter;
+use std::io::stdin;
 
 mod interpreter;
-use interpreter::Interpreter;
 
 fn main() {
+    if let Err(error) = run() {
+        eprintln!("ran into an error: {:#}", error);
+    }
+}
+
+fn run() -> anyhow::Result<()> {
     let mut code = String::new();
     println!("Brainf code:");
-    stdin().read_line(&mut code).unwrap();
+    stdin()
+        .read_line(&mut code)
+        .context("failed reading brainf code")?;
 
     let mut input = String::new();
     println!("Inputs for the program:");
-    stdin().read_line(&mut input).unwrap();
+    stdin()
+        .read_line(&mut input)
+        .context("failed reading program input")?;
+    let mut input = input.as_bytes().iter().copied().cycle();
 
-    let mut program = Interpreter {
-        instructions: Vec::new(),
-        pointer: 0,
-        code,
-        loops: HashMap::new(),
-        input: input.trim().to_string(),
-        tape: [0; 3000]
-    };
+    let mut program = Interpreter::new(&code).context("failed building interpreter")?;
 
-    program.build_instructions();
-    program.run();
+    program.run(&mut input);
+
+    Ok(())
 }
